25f050faae4fe5f0056a2f770997f943abf66c71
Vibs <vibsk02@gmail.com>
2025-05-15 22:19:58 -0500

    Fixed spacing

--- DownloadCleaner.py
+++ DownloadCleaner.py
@@ -2,7 +2,7 @@ from Files import FileEditor
 import os
 import shutil
 
-drive = input("Enter the drive whose download folder you want to clean")
+drive = input("Enter the drive whose download folder you want to clean? ")
 user_folder = input("Enter the name of your folder who's downloads you want to clear out? ")
 cleaner = FileEditor(os.path.join(drive + "\\", "Users", user_folder, "Downloads"))
 cleaner.createNewFolders()
