dddb152b7c0954b904da0dca68aa125f1189f953
Vibs <vibsk02@gmail.com>
2025-05-15 22:04:11 -0500

    Fixed punctuation

--- DownloadCleaner.py
+++ DownloadCleaner.py
@@ -2,7 +2,7 @@ from Files import FileEditor
 import os
 import shutil
 
-user_folder = input("Enter the name of your folder who's downloads you want to clear out")
+user_folder = input("Enter the name of your folder who's downloads you want to clear out? ")
 cleaner = FileEditor(os.path.join("C:", "Users", user_folder, "Downloads"))
 cleaner.createNewFolders()
 cleaner.move()
