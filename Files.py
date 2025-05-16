72e26dc75c0aca68b810a8082b2e8b8b348e2c9d
Vibs <vibsk02@gmail.com>
2025-05-15 22:32:11 -0500

    Changed commas to periods

--- Files.py
+++ Files.py
@@ -19,11 +19,11 @@ class FileEditor:
     
     def move(self):
         for file in os.listdir(os.path.join(self.storage, "Downloads")):
-            if(file.split(",")[1] in ("png" or "jpg" or "bmp" or "gif")):
+            if(file.split(".")[1] in ("png" or "jpg" or "bmp" or "gif")):
                 shutil.move(os.path.join(self.storage, "Downloads"), os.path.join(self.storage, "Pictures"))
-            elif(file.split(",")[1] in ("mp4", "mov", "webm", "mpg", "ogg", "avi", "mov", "flv")):
+            elif(file.split(".")[1] in ("mp4", "mov", "webm", "mpg", "ogg", "avi", "mov", "flv")):
                 shutil.move(os.path.join(self.storage, "Downloads"), os.path.join(self.storage, "Videos"))
-            elif(file.split(",")[1] in ("pdf", "docx", "docm", "doc", "odt", "ppt")):
+            elif(file.split(".")[1] in ("pdf", "docx", "docm", "doc", "odt", "ppt")):
                 shutil.move(os.path.join(self.storage, "Downloads"), os.path.join(self.storage, "Documents"))
             else:
                 shutil.move(os.path.join(self.storage, "Downloads"), os.path.join(self.storage, "Others"))
